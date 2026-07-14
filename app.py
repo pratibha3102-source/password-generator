bc7a0fe013a4c84ccd87db92a5bc215b6febfb9d
hellotejas <pratibha3102@gmail.com>
2026-07-14 22:11:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import random
+import string
+
+st.title("password generator")
+
+length = st.slider("password length", 8, 32, 16)
+chars= string.ascii_letters + string.digits + "@$#&!^&*"
+if st.button("Generate Passwordd"):
+    password= "".join(random.choice(chars) for _ in range (length))
+    st.code(password)
